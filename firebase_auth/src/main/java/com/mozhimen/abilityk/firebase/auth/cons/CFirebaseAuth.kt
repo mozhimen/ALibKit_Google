4036751d89ef1944b63da586705ad1dcaad95bd3
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-01-10 23:12:12 +0800

    0110

---
+++ firebase_auth/src/main/java/com/mozhimen/abilityk/firebase/auth/cons/CFirebaseAuth.kt
@@ -0,0 +1,12 @@
+package com.mozhimen.abilityk.firebase.auth.cons
+
+/**
+ * @ClassName CFirebaseAuth
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/1/10
+ * @Version 1.0
+ */
+object CFirebaseAuth {
+    const val REQUEST_ONE_TAP = 2// Can be any integer unique to the Activity
+}
---
+++ firebase_auth/src/main/java/com/mozhimen/abilityk/firebase/auth/mos/AuthGoogleRes.kt
@@ -0,0 +1,19 @@
+package com.mozhimen.abilityk.firebase.auth.mos
+
+/**
+ * @ClassName AuthGoogleRes
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/1/10
+ * @Version 1.0
+ */
+data class AuthGoogleRes(
+    val isAnonymous: Boolean?,
+    val tenantId: String?,
+    val displayName: String?,
+    val email: String?,
+    val phoneNumber: String?,
+    val photoUrl: String?,
+    val providerId: String?,
+    val uid: String?
+)
