419f741170c8e10eb70f124ed06625520f600a5e
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-05 20:16:03 +0800

    0605

---
+++ android_gms_play_ads/src/main/java/com/mozhimen/abilityk/google/android/gms/play/ads/optins/OMetaData_GMS_ADS_APPLICATION_ID.kt
@@ -0,0 +1,15 @@
+package com.mozhimen.abilityk.google.android.gms.play.ads.optins
+
+import com.mozhimen.abilityk.google.android.gms.play.ads.cons.CMetaData
+import com.mozhimen.basick.manifestk.annors.AManifestKRequire
+
+/**
+ * @ClassName OMetaData_APPLICATION_ID
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/2/4
+ * @Version 1.0
+ */
+@AManifestKRequire(CMetaData.GMS_ADS_APPLICATION_ID)
+@RequiresOptIn("The api is must add this metadata to your AndroidManifest.xml. 需要声明此Metadata到你的AndroidManifest.xml", RequiresOptIn.Level.WARNING)
+annotation class OMetaData_GMS_ADS_APPLICATION_ID
