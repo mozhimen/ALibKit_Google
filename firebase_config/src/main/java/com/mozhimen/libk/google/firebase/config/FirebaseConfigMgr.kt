444b9584dfee376121ce9d4db58f030f453064c0
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-07-04 21:18:06 +0800

    0704

--- firebase_config/src/main/java/com/mozhimen/libk/google/firebase/config/FirebaseConfigMgr.kt
+++ firebase_config/src/main/java/com/mozhimen/libk/google/firebase/config/FirebaseConfigMgr.kt
@@ -1,9 +1,21 @@
 package com.mozhimen.libk.google.firebase.config
 
+import androidx.annotation.IntRange
+import androidx.annotation.XmlRes
+import com.google.android.gms.tasks.Task
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigValue
+import com.google.firebase.remoteconfig.ktx.get
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.mozhimen.basick.elemk.commons.IA_Listener
+import com.mozhimen.basick.elemk.commons.I_Listener
+import com.mozhimen.basick.utilk.android.util.UtilKLogWrapper
+import com.mozhimen.basick.utilk.commons.IUtilK
 import com.mozhimen.libk.google.firebase.basic.optins.OApiInit_InApplication_FirebaseBasicMgr
 
 /**
@@ -13,26 +25,105 @@ import com.mozhimen.libk.google.firebase.basic.optins.OApiInit_InApplication_Fir
  * @Date 2024/6/27
  * @Version 1.0
  */
-object FirebaseConfigMgr {
+object FirebaseConfigMgr : IUtilK {
+    private val _firebaseConfig: FirebaseRemoteConfig by lazy {
+        // [START shared_app_measurement]
+        // Obtain the FirebaseAnalytics instance.
+        Firebase.remoteConfig
+        // [END shared_app_measurement]
+    }
+    val firebaseConfig get() = _firebaseConfig
+
+    private val _onConfigUpdateListeners: MutableList<IA_Listener<ConfigUpdate>> = mutableListOf()
+
+    //////////////////////////////////////////////////////////////////////
+
     /**
      * 默认10分钟刷新一次
      */
     @JvmStatic
     @OApiInit_InApplication_FirebaseBasicMgr
-    fun init(minimumFetchIntervalInSeconds: Long = 600L) {
-        // Get Remote Config instance.
-        // [START get_remote_config_instance]
-        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
-        // [END get_remote_config_instance]
-
-        // Create a Remote Config Setting to enable developer mode, which you can use to increase
-        // the number of fetches available per hour during development. Also use Remote Config
-        // Setting to set the minimum fetch interval.
-        // [START enable_dev_mode]
+    fun init(@IntRange(from = 600L) minimumFetchIntervalInSeconds: Long = 600L) {
         val configSettings = remoteConfigSettings {
             setMinimumFetchIntervalInSeconds(minimumFetchIntervalInSeconds)
         }
-        remoteConfig.setConfigSettingsAsync(configSettings)
-        // [END enable_dev_mode]
+        _firebaseConfig.setConfigSettingsAsync(configSettings)
     }
+
+    //////////////////////////////////////////////////////////////////////
+
+    @JvmStatic
+    fun setDefaultsAsync(@XmlRes intResXml: Int) {
+        _firebaseConfig.setDefaultsAsync(intResXml)
+    }
+
+    @JvmStatic
+    fun addOnConfigUpdateListener(listener: IA_Listener<ConfigUpdate>) {
+        if (_onConfigUpdateListeners.isEmpty()) {
+            _firebaseConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+                override fun onUpdate(configUpdate: ConfigUpdate) {
+                    _onConfigUpdateListeners.forEach { it.invoke(configUpdate) }
+                }
+
+                override fun onError(error: FirebaseRemoteConfigException) {
+                    UtilKLogWrapper.e(TAG, "onError: code ${error.code} cause ${error.cause} msg ${error.message}")
+                }
+            })
+        }
+        _onConfigUpdateListeners.add(listener)
+    }
+
+    @JvmStatic
+    fun removeOnConfigUpdateListener(listener: IA_Listener<ConfigUpdate>) {
+        _onConfigUpdateListeners.remove(listener)
+    }
+
+    @JvmStatic
+    fun clearOnConfigUpdateListeners() {
+        _onConfigUpdateListeners.clear()
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    @JvmStatic
+    fun getFirebaseRemoteConfigValue(key: String): FirebaseRemoteConfigValue =
+        _firebaseConfig[key]
+
+    @JvmStatic
+    fun getString(key: String): String =
+        _firebaseConfig.getString(key)
+
+    @JvmStatic
+    fun getBoolean(key: String): Boolean =
+        _firebaseConfig.getBoolean(key)
+
+    @JvmStatic
+    fun getLong(key: String): Long =
+        _firebaseConfig.getLong(key)
+
+    @JvmStatic
+    fun getDouble(key: String): Double =
+        _firebaseConfig.getDouble(key)
+
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * xxx.addOnCompleteListener(this) { task ->
+     *         if (task.isSuccessful) {
+     *             val updated = task.result
+     *             Log.d(TAG, "Config params updated: $updated")
+     *             Toast.makeText(this,"Fetch and activate succeeded",Toast.LENGTH_SHORT,).show()
+     *         } else {
+     *             Toast.makeText(this,"Fetch failed",Toast.LENGTH_SHORT,).show()
+     *         }
+     *         displayWelcomeMessage()
+     * }
+     */
+    @JvmStatic
+    fun fetchAndActivate(): Task<Boolean> =
+        _firebaseConfig.fetchAndActivate()
+
+    @JvmStatic
+    fun activate(): Task<Boolean> =
+        _firebaseConfig.activate()
 }
---
+++ firebase_config_test/src/main/java/com/mozhimen/libk/google/firebase/config/test/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.mozhimen.libk.google.firebase.config.test
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.remoteconfig.ktx.get
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.mozhimen.basick.elemk.androidx.appcompat.bases.viewbinding.BaseActivityVB
+import com.mozhimen.basick.lintk.optins.OApiInit_InApplication
+import com.mozhimen.basick.utilk.android.widget.showToast
+import com.mozhimen.libk.google.firebase.basic.FirebaseBasicMgr
+import com.mozhimen.libk.google.firebase.basic.optins.OApiInit_InApplication_FirebaseBasicMgr
+import com.mozhimen.libk.google.firebase.basic.optins.OAppRootPath_GoogleServicesJson
+import com.mozhimen.libk.google.firebase.basic.optins.OPlugin_ClassPath_GmsGoogleServices
+import com.mozhimen.libk.google.firebase.config.FirebaseConfigMgr
+import com.mozhimen.libk.google.firebase.config.test.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivityVB<ActivityMainBinding>() {
+
+    companion object {
+        private const val LOADING_PHRASE_CONFIG_KEY = "loading_phrase"
+        private const val WELCOME_MESSAGE_KEY = "welcome_message"
+        private const val WELCOME_MESSAGE_CAPS_KEY = "welcome_message_caps"
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+
+    @OptIn(OApiInit_InApplication_FirebaseBasicMgr::class, OPlugin_ClassPath_GmsGoogleServices::class, OAppRootPath_GoogleServicesJson::class, OApiInit_InApplication::class)
+    override fun initView(savedInstanceState: Bundle?) {
+        vb.fetchButton.setOnClickListener {
+            fetchWelcome()
+        }
+
+        FirebaseBasicMgr.init()
+        FirebaseConfigMgr.init()
+        FirebaseConfigMgr.setDefaultsAsync(R.xml.remote_config_defaults)
+        FirebaseConfigMgr.addOnConfigUpdateListener { configUpdate ->
+            Log.d(TAG, "Updated keys: " + configUpdate.updatedKeys)
+            if (configUpdate.updatedKeys.contains(WELCOME_MESSAGE_KEY)) {
+                FirebaseConfigMgr.activate().addOnCompleteListener {
+                    displayWelcomeMessage()
+                }
+            }
+        }
+        fetchWelcome()
+    }
+
+    private fun fetchWelcome() {
+        displayWelcomeMessage()
+        FirebaseConfigMgr.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+                    "Fetch and activate succeeded".showToast()
+                } else {
+                    "Fetch failed".showToast()
+                }
+                displayWelcomeMessage()
+            }
+    }
+
+    private fun displayWelcomeMessage() {
+        vb.welcomeTextView.isAllCaps = FirebaseConfigMgr.firebaseConfig[WELCOME_MESSAGE_CAPS_KEY].asBoolean()
+        vb.welcomeTextView.text = FirebaseConfigMgr.firebaseConfig[WELCOME_MESSAGE_KEY].asString()
+    }
+}
