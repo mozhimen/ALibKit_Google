2e3b6e004180ce3fd58ada47a91816b5678b700c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-07-16 22:36:13 +0800

    0716

--- firebase_analytics/src/main/java/com/mozhimen/libk/google/firebase/analytics/FirebaseAnalyticsMgr.kt
+++ firebase_analytics/src/main/java/com/mozhimen/libk/google/firebase/analytics/FirebaseAnalyticsMgr.kt
@@ -1,9 +1,7 @@
 package com.mozhimen.libk.google.firebase.analytics
 
-import com.mozhimen.basick.BuildConfig
 import com.mozhimen.basick.lintk.optins.OApiInit_InApplication
 import com.mozhimen.libk.google.firebase.basic.FirebaseBasicMgr
-import com.mozhimen.libk.google.firebase.basic.optins.OApiInit_InApplication_FirebaseBasicMgr
 import com.mozhimen.libk.google.firebase.basic.optins.OAppRootPath_GoogleServicesJson
 import com.mozhimen.libk.google.firebase.basic.optins.OPlugin_ClassPath_GmsGoogleServices
 
@@ -15,14 +13,13 @@ import com.mozhimen.libk.google.firebase.basic.optins.OPlugin_ClassPath_GmsGoogl
  * @Version 1.0
  */
 object FirebaseAnalyticsMgr {
-    @OptIn(OApiInit_InApplication_FirebaseBasicMgr::class)
     @OAppRootPath_GoogleServicesJson
     @OPlugin_ClassPath_GmsGoogleServices
     @OApiInit_InApplication
     @JvmStatic
     fun init() {
         FirebaseBasicMgr.init()
-        if (BuildConfig.DEBUG)
-            FirebaseAnalytics.firebaseAnalytics.setAnalyticsCollectionEnabled(false)
+//        if (BuildConfig.DEBUG)
+//            FirebaseAnalytics.firebaseAnalytics.setAnalyticsCollectionEnabled(false)
     }
 }
